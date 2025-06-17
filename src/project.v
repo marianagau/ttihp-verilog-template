533b09151508209f86a1b01b5ab44dd257166634
marianagau <93737061+marianagau@users.noreply.github.com>
2025-06-17 16:34:09 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,23 +5,23 @@
 
 `default_nettype none
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+module tt_um_equipo7 (
+    input  wire [7:0] ui_in,    // Entradas dedicadas
+    output wire [7:0] uo_out,   // Salidas dedicadas
+    input  wire [7:0] uio_in,   // IOs: Entrada
+    output wire [7:0] uio_out,  // IOs: Salida
+    output wire [7:0] uio_oe,   // IOs: Habilitación (1=salida, 0=entrada)
+    input  wire       ena,      // Siempre en 1 cuando el diseño está encendido
+    input  wire       clk,      // Reloj
+    input  wire       rst_n     // Reset activo en bajo
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    // Ejemplo sencillo: suma de ui_in y uio_in, y salida por uo_out
+    assign uo_out  = ui_in + uio_in;
+    assign uio_out = 8'b00000000;
+    assign uio_oe  = 8'b00000000;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    // Para evitar advertencias por señales no utilizadas
+    wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule
